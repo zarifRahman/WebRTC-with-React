a192b0627b22aef230714db86b91894797e3c277
zarifRahman <zarif.shahriar.rahman@gmail.com>
2021-09-29 15:01:07 +0600

    add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
+// Connection
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
